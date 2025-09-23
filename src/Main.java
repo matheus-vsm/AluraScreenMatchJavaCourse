7bc0896f1db502d04b46dbcb66d2484436ca4f66
matheus-vsm <scompinha@hotmail.com>
2025-09-22 21:56:27 -0300

    aula 1.5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Filme filme = new Filme();
-        System.out.println("Oi");
+
+        filme.nome = "Dragon Ball";
+        filme.anoDeLancamento = 2012;
+        filme.exibeFichaTecnica();
+        filme.avalia(8);
+        filme.avalia(5);
+        filme.avalia(10);
+        System.out.println(filme.somaDasAvaliacoes);
+        System.out.println(filme.totalDeAvaliacoes);
+        System.out.println(filme.obterMedia());
     }
 }
