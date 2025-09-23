afdb0382da347f3331305704a2a580edbea985a9
matheus-vsm <scompinha@hotmail.com>
2025-09-22 22:22:03 -0300

    aula 2.5

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+    public String nome;
+    public int anoDeLancamento;
+    public boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    public int duracaoEmMinutos;
+
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do br.com.alura.screenmatch.modelos.Filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double obterMedia(){
+        return somaDasAvaliacoes/totalDeAvaliacoes;
+    }
+}
