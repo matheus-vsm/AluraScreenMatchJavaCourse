502646c44afaf2f11f7d17b19b1e4636bed4c306
Matheus Venâncio Scomparim <scompinha@hotmail.com>
2025-10-09 09:19:30 -0300

    aula 1.11

--- src/br/com/alura/screenmatch/principal/MainBusca.java
+++ src/br/com/alura/screenmatch/principal/MainBusca.java
@@ -5,15 +5,22 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainBusca {
     static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite um Filme para buscar: ");
+        var busca = input.nextLine();
+
+        String url = "https://www.omdbapi.com/?t=" + busca + "&apikey=6585022c";
+
         // Cria um cliente HTTP usando a API nativa do Java
         HttpClient client = HttpClient.newHttpClient();
 
         // Cria uma requisição HTTP
-        HttpRequest request = HttpRequest.newBuilder() // Aqui usamos o método "newBuilder()" para configurar a requisição.
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=6585022c")) //uri() define o endereço (endpoint) da API que queremos acessar.
+        HttpRequest request = HttpRequest.newBuilder() // Aqui usamos o metodo "newBuilder()" para configurar a requisição.
+                .uri(URI.create(url)) //uri() define o endereço (endpoint) da API que queremos acessar.
                 .build(); // Finaliza a construção da requisição
 
         // Cria uma resposta HTTP
