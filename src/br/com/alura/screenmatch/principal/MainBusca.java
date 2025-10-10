f63c7fe497509d8ddd4993fddca0269e3e1c3e62
matheus-vsm <scompinha@hotmail.com>
2025-10-09 21:52:34 -0300

    aula 3.7

--- src/br/com/alura/screenmatch/principal/MainBusca.java
+++ src/br/com/alura/screenmatch/principal/MainBusca.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.excecao.ErroDeConversaoDeAnoException;
 import br.com.alura.screenmatch.modelos.Titulo;
 import br.com.alura.screenmatch.modelos.TituloOmdb;
 import com.google.gson.FieldNamingPolicy;
@@ -19,29 +20,39 @@ public class MainBusca {
         System.out.print("Digite um Filme para buscar: ");
         var busca = input.nextLine();
 
-        String url = "https://www.omdbapi.com/?t=" + busca + "&apikey=6585022c";
+        String url = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=6585022c";
 
-        // Cria um cliente HTTP usando a API nativa do Java
-        HttpClient client = HttpClient.newHttpClient();
+        try {
+            // Cria um cliente HTTP usando a API nativa do Java
+            HttpClient client = HttpClient.newHttpClient();
 
-        // Cria uma requisição HTTP
-        HttpRequest request = HttpRequest.newBuilder() // Aqui usamos o metodo "newBuilder()" para configurar a requisição.
-                .uri(URI.create(url)) //uri() define o endereço (endpoint) da API que queremos acessar.
-                .build(); // Finaliza a construção da requisição
+            // Cria uma requisição HTTP
+            HttpRequest request = HttpRequest.newBuilder() // Aqui usamos o metodo "newBuilder()" para configurar a requisição.
+                    .uri(URI.create(url)) //uri() define o endereço (endpoint) da API que queremos acessar.
+                    .build(); // Finaliza a construção da requisição
 
-        // Cria uma resposta HTTP
-        // O metodo "send" é síncrono (aguarda o servidor responder)
-        // "HttpResponse.BodyHandlers.ofString()" indica que queremos o corpo da resposta como uma String
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String json = response.body();
-        System.out.println(json);
+            // Cria uma resposta HTTP
+            // O metodo "send" é síncrono (aguarda o servidor responder)
+            // "HttpResponse.BodyHandlers.ofString()" indica que queremos o corpo da resposta como uma String
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String json = response.body();
+            System.out.println(json);
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE) // Define a política de nomenclatura, os nomes dos campos começarão com letra maiúscula (ex: "Title" em vez de "title")
-                .create(); // Cria a instância configurada do Gson
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println(meuTitulo);
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE) // Define a política de nomenclatura, os nomes dos campos começarão com letra maiúscula (ex: "Title" em vez de "title")
+                    .create(); // Cria a instância configurada do Gson
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e) {
+            System.out.println(STR."Aconteceu um erro: \{e.getMessage()}");
+        } catch (IllegalArgumentException e){
+            System.out.println("Algum erro foi detectado. Verifique o endereço.");
+        } catch (ErroDeConversaoDeAnoException e){
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Finalizou!");
+        }
     }
 }
